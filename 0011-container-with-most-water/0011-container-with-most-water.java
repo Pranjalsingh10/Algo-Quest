49b050fc5571ddad8224fc43172bfdf56a39ab87
Pranjal Singh <pranjalsingh2508@gmail.com>
2026-07-06 20:59:42 +0530

    Time: 5 ms (82.37%), Space: 77.2 MB (68.49%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,33 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1; // Direct array length
-        int maxproduct = 0;
-        
-        while (left < right) {
-            // The indices themselves ARE the keys!
-            int leftkey = left;
-            int rightkey = right;
-            
-            int difference = rightkey - leftkey;
-            
-            // The array values ARE the map values!
-            int leftval = height[left];
-            int rightval = height[right];
-            
-            int minValue = Math.min(leftval, rightval);
-            int currentProduct = difference * minValue;
-            
-            maxproduct = Math.max(maxproduct, currentProduct);
-            
-            // Your excellent pointer movement logic
-            if (leftval < rightval) {
-                left++;
-            } else {
-                right--;
-            }
+        int h=height.length;
+        int left=0;
+        int right=h-1;
+        int maxarea=0;
+        while(left<right){
+            int width=right-left;
+            int currheight=Math.min(height[left],height[right]);
+           int area=width*currheight;
+           maxarea=Math.max(area,maxarea);
+           if(height[left]<height[right]){
+            left++;
+           } else right--;
+
         }
-        
-        return maxproduct;
+        return maxarea;
     }
 }
