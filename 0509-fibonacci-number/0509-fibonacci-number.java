2185480e76a41eca90666191ad4e8f6e9fb149a6
Pranjal Singh <pranjalsingh2508@gmail.com>
2026-08-08 08:48:13 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,14 @@
 class Solution {
-    public int fib(int n) {//18
-        if(n==0){return 0;}
-        if(n==1){return 1;}
-        if(n==2){return 1;}
-        int[]arr=new int [n+1];
-        arr[0]=0;
-        arr[1]=1;
+    public int fib(int n) {
+        if(n<=1) return n;
+        int zero=0;
+        int one=1;
+        int result=0;
         for(int i=2;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
+           result= zero+one;
+             zero=one;
+            one=result;
         }
-        int right=0;
-        for(int a: arr){
-            right=Math.max(right,a);
-        }
-        return right;
+        return result;
     }
 }
